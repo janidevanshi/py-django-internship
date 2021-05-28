960f51a0f5612287f791160b39f668b42405bbc7
Devanshi Jani <devanshijani169@gmail.com>
2021-05-28 13:56:26 +0530

    smallest of three numbers

---
+++ day3/program13.py
@@ -0,0 +1,15 @@
+# Take 3 numbers and find smallest number using logical operator.
+a = int(input('Enter first number  : '))
+b = int(input('Enter second number : '))
+c = int(input('Enter third number  : '))
+
+smallest = 0
+
+if a < b and a < c:
+    smallest = a
+elif b < c:
+    smallest = b
+else:
+    smallest = c
+
+print(smallest, "is the smallest of three numbers.")
