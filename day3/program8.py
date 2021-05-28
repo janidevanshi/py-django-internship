de5f96a6d3fc9e060552e087fe2259fa4cb5efdf
Devanshi Jani <devanshijani169@gmail.com>
2021-05-28 13:35:56 +0530

    find the smallest number

---
+++ day3/program8.py
@@ -0,0 +1,10 @@
+# Find the smallest number
+a = int(input("Enter number "))
+b = int(input("Enter number "))
+
+if(a < b):
+    print(a, "is smaller than", b)
+elif(a == b):
+    print("Both the numbers are equal")
+else:
+    print(b, "is smaller than", b)
