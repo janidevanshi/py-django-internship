0beccb2166a7010bb55e66e814c91aaf860b7dbd
Devanshi Jani <devanshijani169@gmail.com>
2021-05-28 13:32:34 +0530

    swaping of two numbers

---
+++ day3/program7.py
@@ -0,0 +1,9 @@
+# Write program to swap two numbers using third number
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+
+c = a
+a = b
+b = c
+print("The first number after swaping is", a)
+print("The second number after swaping is", b)
