66934e4ee5114039f110c88b7b6adac05073536b
Devanshi Jani <devanshijani169@gmail.com>
2021-05-28 13:47:50 +0530

    nested if else

---
+++ day3/program10.py
@@ -0,0 +1,8 @@
+# Take a number to print the square of a number if it is less than 10.
+a = int(input("Enter Number"))
+if(a < 10):
+    print(a, "is less than 10")
+    print(a*a, "is square of ", a)
+
+else:
+    print(a, "is greater than 10")
